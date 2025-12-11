de237f4749071bb849925a19b43e7078af4bf577
Thaykoo <matheo.delcroix13@gmail.com>
2025-12-11 11:54:52 +0100

    fix: correct RAM command - fixed grep pattern from 'Memm' to 'Mem'

--- monitor.sh
+++ monitor.sh
@@ -11,9 +11,9 @@ echo "--- CPU Usage ---"
 top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print "CPU Usage: " 100 - $1"%"}'
 echo ""
 
-# RAM Usage (ERREUR VOLONTAIRE : commande incorrecte)
+# RAM Usage
 echo "--- RAM Usage ---"
-free -h | grep Memm | awk '{print "RAM Usage: " $3 "/" $2}'
+free -h | grep Mem | awk '{print "RAM Usage: " $3 "/" $2}'
 echo ""
 
 # Disk Space Usage
